c69f905d587db56c228bb39ac287b3f58ea291a7
Czilasy <czyooutzilas@gmail.com>
2026-08-31 15:27:07 +0800

    build: 版本提升至 1.8.4（031 工程还债收口）

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 本文件记录 Career Scout 的所有重要变更。格式参考 [Keep a Changelog](https://keepachangelog.com/zh-CN/1.1.0/)，版本号遵循 [语义化版本](https://semver.org/lang/zh-CN/)。
 
+## [1.8.4] - 2026-08-31
+
+修复：
+- 常规发布
+
 ## [1.8.3] - 2026-08-29
 
 修复：
--- tests/test_desktop_shell.py
+++ tests/test_desktop_shell.py
@@ -502,7 +502,7 @@ class ReadVersionTests(unittest.TestCase):
 
     def test_reads_version_from_pyproject(self):
         version = desktop.read_version()
-        self.assertEqual(version, "1.8.3")
+        self.assertEqual(version, "1.8.4")
 
     def test_read_version_returns_string(self):
         self.assertIsInstance(desktop.read_version(), str)
